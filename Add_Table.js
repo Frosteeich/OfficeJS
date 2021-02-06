d69fee09359fe673002d66411f6c2d99d9ed8919
Frosteeich <frosteekosovich@gmail.com>
2021-02-06 12:36:27 +0000

    Update Add_Table.js

--- Add_Table.js
+++ Add_Table.js
@@ -1,20 +1,35 @@
-// Working snippet
-Excel.run(function (context) {
-    var sheet = context.workbook.worksheets.getItem("Items");
-    var ItemsTable = sheet.tables.add(WorkingArea)
-    ItemsTable.name = "MEWS_Items";
-}).catch(errorHandlerFunction);
-
-// Working snippet
-
+// Working add range
 function main(workbook: ExcelScript.Workbook) {
   // Get the currently used range.
-  var range = workbook.getActiveWorksheet().getUsedRange();
   var selectedSheet = workbook.getActiveWorksheet();
+  var range = workbook.getActiveWorksheet().getUsedRange();
   var MEWSItems_table = selectedSheet.addTable(range, true);
-  MEWSItems_table.setName("MEWSItems_table");
-    MEWSItems_table.addColumn(null /*add columns to the end of the table*/, [
-      ["Type of the Day"],
-      ['=IF(OR((TEXT([Consumed], "dddd") = "Saturday"), (TEXT([Consumed],"dddd") = "Sunday")), "Weekend", "Weekday")'],
-    ]);
+}
+
+// Working apply filter
+function main(workbook: ExcelScript.Workbook) {
+  let MEWSItems_table = workbook.getTable("MEWSItems_table");
+  // Apply filter
+  MEWSItems_table.getColumnByName("Accounting category")
+    .getFilter()
+    .applyCustomFilter("=*910 Adyen*");
+}
+
+// Working clear filter
+function main(workbook: ExcelScript.Workbook) {
+  let MEWSItems_table = workbook.getTable("MEWSItems_table");
+  // Clear filter
+  MEWSItems_table.clearFilters();
+}
+
+//Working sort
+
+function main(workbook: ExcelScript.Workbook) {
+  let MEWSItems_table = workbook.getTable("MEWSItems_table");
+  // Sort on table: 'MEWSItems_table' column index: '11'
+  MEWSItems_table.getSort()
+    .apply([{
+      key: 11,
+      ascending: true
+    }]);
 }
