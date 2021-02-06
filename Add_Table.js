4b712e9b640ab36c421c22c01f2289ee390ff29b
Frosteeich <frosteekosovich@gmail.com>
2021-02-06 14:43:16 +0000

    Update Add_Table.js

--- Add_Table.js
+++ Add_Table.js
@@ -31,5 +31,26 @@ function main(workbook: ExcelScript.Workbook) {
     .apply([{
       key: 11,
       ascending: true
-    }]);
+    }]); 
+}
+
+//Add consumed date value
+function main(workbook: ExcelScript.Workbook) {
+  let selectedSheet = workbook.getActiveWorksheet();
+  // Insert at range M:M, move existing cells right
+  selectedSheet.getRange("M:M")
+    .insert(ExcelScript.InsertShiftDirection.right);
+  // Set range M1:M2 on selectedSheet
+  selectedSheet.getRange("M1:M2")
+    .setValues([
+      ["Consumed Date Value"],
+      ["=value(text(L2,\"dd/mm/yyyy\"))"]
+    ]);
+  // Set number formats for Items!M:M
+  selectedSheet.getRange("M:M")
+    .setNumberFormatLocal("dd/mm/yyyy");
+  // Set width of column(s) M1 to 141.75
+  selectedSheet.getRange("M1")
+    .getFormat()
+    .setColumnWidth(141.75);
 }
